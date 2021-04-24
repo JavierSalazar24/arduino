61e8bc753886f599fda6f68b8a7f81af369cee7c
Javier Salazar <javieralejandrosalazartorres@gmail.com>
2021-04-23 23:56:17 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const uri =
 //Escuchar por el puerto que nos de el sitio
 const puerto = process.env.PORT || 3900;
 
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
 
 app.listen(puerto, () => {
   console.log(
