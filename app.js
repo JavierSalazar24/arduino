9b7866ae2db5edf8825b4e9af1e432ef4748a4f2
Javier Salazar <javieralejandrosalazartorres@gmail.com>
2021-04-25 16:20:49 -0500

    make it better

--- app.js
+++ app.js
@@ -2,9 +2,14 @@
 
 //ejecutar el express
 var bodyParser = require("body-parser"); //convertir todo a JSON
-const express = require("express");
-const app = express();
-// const rutas = express.Router();
+const express = require("express"); //requerir express
+const app = express(); //instanciar express
+const Serialport = require("serialport"); //Importación de la libreria del serialport
+
+//configuración del puerto del arduino y los baudios establecidos en el Arduino IDE
+const port = new Serialport("COM3", {
+  baudRate: 57600,
+});
 
 // cargar el body-parser para utilizar posteriormente
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,198 +33,21 @@ app.listen(puerto, () => {
 });
 
 // // Traer el board y el led
-var { Board, Led, Proximity } = require("johnny-five");
-var board = new Board();
+var five = require("johnny-five");
+var firmata = require("firmata");
 
-app.get("/", (req, res) => {
-  let mensaje = "Hola mundo";
-  res.send(
-    board.on("ready", async () => {
-      console.log("Probando con board");
-      var led = new Led(13);
-      var buzzer = new Led(12);
-
-      // var params = req.body;
-      // let id = parseInt(params.id_alarma) + 1;
-
-      proximity = new Proximity({
-        controller: "HCSR04",
-        pin: 3,
-      });
-
-      await proximity.on("data", async function () {
-        if (this.cm <= 20 && this.cm > 0) {
-          console.log(" cm : ", this.cm);
-          led.on();
-          buzzer.on();
-
-          // connect();
-          // async function connect() {
-          //   const client = new MongoClient(uri, {
-          //     useNewUrlParser: true,
-          //     useUnifiedTopology: true,
-          //   });
-          //   await client.connect();
-          //   const db = client.db("opss");
-          //   // console.log("conectado a la BD", db.databaseName);
-          //   const clientes = db.collection("clientes");
-          //   await clientes.updateOne(
-          //     { correo: "javier@gmail.com" },
-          //     { $set: { [`alarmas.alarma 1`]: "encendida" } }
-          //     // { correo: params.correo },
-          //     // { $set: { [`alarmas.alarma ${id}`]: "encendida" } }
-          //   );
-          //   // console.log(actualizar.modifiedCount);
-          //   client.close();
-          // }
-        }
-        led.off();
-        buzzer.off();
-      });
-
-      // // Ruta de encendido
-      // app.get("/encendido", (req, res) => {
-      //   status = true;
-      //   console.log(status);
-
-      //   led.blink(500);
-      //   buzzer.blink(500);
-
-      //   return res.status(200).send({
-      //     status,
-      //   });
-      // });
-
-      // // Ruta de apagado
-      // app.get("/apagado", (req, res) => {
-      //   status = false;
-      //   console.log(status);
-
-      //   led.stop();
-      //   led.off();
-      //   buzzer.off();
-      //   buzzer.stop();
-
-      //   return res.status(200).send({
-      //     status,
-      //   });
-
-      //   // var params = req.body;
-      //   // let id = parseInt(params.id_alarma) + 1;
-
-      //   // connect();
-      //   // async function connect() {
-      //   //   const client = new MongoClient(uri, {
-      //   //     useNewUrlParser: true,
-      //   //     useUnifiedTopology: true,
-      //   //   });
-      //   //   await client.connect();
-      //   //   const db = client.db("opss");
-      //   //   // console.log("conectado a la BD", db.databaseName);
-      //   //   const clientes = db.collection("clientes");
-      //   //   await clientes.updateOne(
-      //   //     { correo: params.correo },
-      //   //     { $set: { [`alarmas.alarma ${id}`]: "apagada" } }
-      //   //   );
-      //   //   // console.log(actualizar.modifiedCount);
-      //   //   client.close();
-      //   // }
-      // });
-    })
-  );
-  // Iniciar el board
-  // board.on("ready", async () => {
-  //   console.log("Probando con board");
-  //   var led = new Led(13);
-  //   var buzzer = new Led(12);
-
-  //   // var params = req.body;
-  //   // let id = parseInt(params.id_alarma) + 1;
-
-  //   proximity = new Proximity({
-  //     controller: "HCSR04",
-  //     pin: 3,
-  //   });
-
-  //   await proximity.on("data", async function () {
-  //     if (this.cm <= 20 && this.cm > 0) {
-  //       console.log(" cm : ", this.cm);
-  //       led.on();
-  //       buzzer.on();
-
-  //       // connect();
-  //       // async function connect() {
-  //       //   const client = new MongoClient(uri, {
-  //       //     useNewUrlParser: true,
-  //       //     useUnifiedTopology: true,
-  //       //   });
-  //       //   await client.connect();
-  //       //   const db = client.db("opss");
-  //       //   // console.log("conectado a la BD", db.databaseName);
-  //       //   const clientes = db.collection("clientes");
-  //       //   await clientes.updateOne(
-  //       //     { correo: "javier@gmail.com" },
-  //       //     { $set: { [`alarmas.alarma 1`]: "encendida" } }
-  //       //     // { correo: params.correo },
-  //       //     // { $set: { [`alarmas.alarma ${id}`]: "encendida" } }
-  //       //   );
-  //       //   // console.log(actualizar.modifiedCount);
-  //       //   client.close();
-  //       // }
-  //     }
-  //     led.off();
-  //     buzzer.off();
-  //   });
-
-  //   // // Ruta de encendido
-  //   // app.get("/encendido", (req, res) => {
-  //   //   status = true;
-  //   //   console.log(status);
-
-  //   //   led.blink(500);
-  //   //   buzzer.blink(500);
-
-  //   //   return res.status(200).send({
-  //   //     status,
-  //   //   });
-  //   // });
-
-  //   // // Ruta de apagado
-  //   // app.get("/apagado", (req, res) => {
-  //   //   status = false;
-  //   //   console.log(status);
-
-  //   //   led.stop();
-  //   //   led.off();
-  //   //   buzzer.off();
-  //   //   buzzer.stop();
-
-  //   //   return res.status(200).send({
-  //   //     status,
-  //   //   });
-
-  //   //   // var params = req.body;
-  //   //   // let id = parseInt(params.id_alarma) + 1;
-
-  //   //   // connect();
-  //   //   // async function connect() {
-  //   //   //   const client = new MongoClient(uri, {
-  //   //   //     useNewUrlParser: true,
-  //   //   //     useUnifiedTopology: true,
-  //   //   //   });
-  //   //   //   await client.connect();
-  //   //   //   const db = client.db("opss");
-  //   //   //   // console.log("conectado a la BD", db.databaseName);
-  //   //   //   const clientes = db.collection("clientes");
-  //   //   //   await clientes.updateOne(
-  //   //   //     { correo: params.correo },
-  //   //   //     { $set: { [`alarmas.alarma ${id}`]: "apagada" } }
-  //   //   //   );
-  //   //   //   // console.log(actualizar.modifiedCount);
-  //   //   //   client.close();
-  //   //   // }
-  //   // });
-  // });
+//The "sp" variable here could be any instance of a virtual serial port
+var io = new firmata.Board(port);
+var board = new five.Board({ io: io });
+board.on("ready", function () {
+  var led = new five.Led(13);
+  app.get("/", (req, res) => {
+    let mensaje = "Hola mundo";
+    res.send({
+      mensaje,
+    });
+    led.on();
+  });
 });
 
 // // Iniciar el board
